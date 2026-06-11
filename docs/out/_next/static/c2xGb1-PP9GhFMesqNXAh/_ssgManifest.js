820fbf35610d4fea80c6e9fc3c509e7846b674b2
Hien Nguyen Q. <hiennq02@outlook.com>
2026-06-11 10:00:17 +0700

    update basePath

---
+++ docs/out/_next/static/c2xGb1-PP9GhFMesqNXAh/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fflight-info","\u002F[locale]\u002Fofficial-updates","\u002F[locale]\u002Fpress-releases","\u002F[locale]\u002Fsupport"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
